4a29d9a12ce72042c492b10bd640dae934fe316c
Michael Rivera <mirivera@student.42.us.org>
2021-09-18 14:08:45 -0700

    dom manipulation solution

---
+++ exercises/4.2.17-solution/dom-manipulation-exercise.js
@@ -0,0 +1,56 @@
+// Exercise
+// Write the code necessary to do the following:
+
+// [✓] Select the section with an id of container without using querySelector.
+// const container = document.getElementById("container");
+// console.log(container);
+
+// [✓] Select the section with an id of container using querySelector.
+// const container = document.querySelector("#container");
+// console.log(container);
+
+// [✓] Select all of the list items with a class of “second”.
+// const allList = document.querySelectorAll(".second");
+// console.log(allList);
+
+// [✓] Select a list item with a class of third, but only the list item inside of the ol tag.
+// const ol = document.querySelector("ol");
+// const thirdOl = ol.querySelector(".third");
+// console.log(thirdOl);
+
+// [✓] Give the section with an id of container the text “Hello!”.
+// const container = document.getElementById("container");
+// console.log(container);
+// container.innerText = "Hello!";
+
+// [✓] Add the class main to the div with a class of footer.
+// const footer = document.querySelector(".footer");
+// footer.classList.add("main");
+// console.log(footer);
+
+// [✓] Remove the class main on the div with a class of footer.
+// const footer = document.querySelector(".footer");
+// footer.classList.add("main");
+// footer.classList.remove("main");
+
+// [✓] Create a new li element.
+// const newLi = document.createElement("li");
+
+// [✓] Give the li the text “four”.
+// newLi.innerText = "four";
+
+// [✓] Append the li to the ul element.
+// const ul = document.querySelector("ul");
+// ul.append(newLi);
+
+// [✓] Loop over all of the lis inside the ol tag and give them a background color of “green”.
+// const ol = document.querySelector("ol");
+// const olLis = ol.querySelectorAll("li");
+
+// for (let lis of olLis) {
+//   lis.style.backgroundColor = "green";
+// }
+
+// [✓] Remove the div with a class of footer
+// const footer = document.querySelector(".footer");
+// footer.remove();
