635e9d507c6263e2507e1c56ddc15108fe3e1e36
Michael Rivera <mirivera@student.42.us.org>
2021-09-23 19:47:48 -0700

    bare bones todo-list is working

--- exercises/4.4.6-solution/todo-app.js
+++ exercises/4.4.6-solution/todo-app.js
@@ -8,7 +8,7 @@ As a user, you should be able to:
 [✓] Mark a todo as completed (cross out the text of the todo)
 [✓] Remove a todo
 Part 2
-[ ] Now that you have a functioning todo app, save your todos in localStorage! Make sure that when the page refreshes, the todos on the page remain there.
+[✓] Now that you have a functioning todo app, save your todos in localStorage! Make sure that when the page refreshes, the todos on the page remain there.
 
 to add to localstorage and remove from local storage:
 make an empty array in your code
@@ -23,6 +23,7 @@ show items first when opening page with getItem
 resources:
 push(): https://www.w3schools.com/jsref/jsref_push.asp
 pop(): https://www.w3schools.com/jsref/jsref_pop.asp
+https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array#5767357
 
 */
 
@@ -32,73 +33,62 @@ const newTodoInput = document.querySelector('input[name="newTodo"]');
 const todoList = document.querySelector("ul");
 
 // create an empty array of strings for localStorage
-let todoStorage = ["test"];
+let todoStorage = [];
 
-// if ((todoStorage = JSON.parse(localStorage.getItem("todoStorage")))) {
-//   console.log(
-//     "This is your array in localstorage: ",
-//     JSON.parse(localStorage.getItem("todoStorage"))
-//   );
-//   todoStorage = JSON.parse(localStorage.getItem("todoStorage"));
-//   if (todoStorage !== null) {
-//     for (todo of todoStorage) {
-//       const newTodo = document.createElement("li");
-//       newTodo.innerText = `${todo}    `;
-//       const delButton = document.createElement("button");
-//       delButton.innerText = "X";
-//       newTodo.append(delButton);
-//       todoList.append(newTodo);
-//     }
-//   }
-// }
-// add those strings to the ul in the DOM
-const getStorageTodos = () => {
-  console.log(
-    "This is your array in localstorage: ",
-    JSON.parse(localStorage.getItem("todoStorage"))
-  );
+// add strings in local storage to the ul in the DOM
+console.log(
+  "This is your array in localstorage: ",
+  JSON.parse(localStorage.getItem("todoStorage"))
+);
+if (JSON.parse(localStorage.getItem("todoStorage")) !== null) {
   todoStorage = JSON.parse(localStorage.getItem("todoStorage"));
-  if (todoStorage !== null) {
-    for (todo of todoStorage) {
-      const newTodo = document.createElement("li");
-      newTodo.innerText = `${todo}    `;
-      const delButton = document.createElement("button");
-      delButton.innerText = "X";
-      newTodo.append(delButton);
-      todoList.append(newTodo);
-    }
+  for (todo of todoStorage) {
+    const newTodo = document.createElement("li");
+    newTodo.innerText = `${todo}    `;
+    const delButton = document.createElement("button");
+    delButton.innerText = "X";
+    // newTodo.append(delButton);
+    todoList.append(newTodo);
+    todoList.append(delButton);
   }
-};
+}
 
-getStorageTodos();
 // add a newtodo by submitting the form
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const newTodo = document.createElement("li");
-  newTodo.innerText = `${newTodoInput.value}    `;
-  // add newTodo string to array
-  todoStorage.push(newTodoInput.value);
-  // add updated todo array to localStorage
-  localStorage.setItem("todoStorage", JSON.stringify(todoStorage));
-  const delButton = document.createElement("button");
-  delButton.innerText = "X";
-  newTodo.append(delButton);
-  todoList.append(newTodo);
-  newTodoInput.value = "";
+  console.log(newTodoInput.value);
+  if (newTodoInput.value !== "") {
+    newTodo.innerText = `${newTodoInput.value}    `;
+    // add newTodo string to array
+    todoStorage.push(newTodoInput.value);
+    // add updated todo array to localStorage
+    localStorage.setItem("todoStorage", JSON.stringify(todoStorage));
+    const delButton = document.createElement("button");
+    delButton.innerText = "X";
+    // newTodo.append(delButton);
+    todoList.append(newTodo);
+    todoList.append(delButton);
+    newTodoInput.value = "";
+  } else {
+    alert("Please enter a task!");
+  }
 });
 
 todoList.addEventListener("click", function (event) {
   // remove a todo
   if (event.target.tagName === "BUTTON") {
-    event.target.parentElement.remove();
-    // remove the item from localstorage using this: https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array#5767357
-
-    // xxxxxxx
+    const todoToRemove = event.target.previousElementSibling.innerText;
+    todoStorage = todoStorage.filter(function (item) {
+      return item !== todoToRemove;
+    });
+    console.log(todoStorage);
+    localStorage.setItem("todoStorage", JSON.stringify(todoStorage));
+    event.target.previousElementSibling.remove();
+    event.target.remove();
   }
   // add a completed class to todo
   if (event.target.tagName === "LI") {
-    // console.log(event.target.classList);
     event.target.classList.toggle("complete");
   }
-  // save your todos in an object in localStorage to be read upon refresh
 });
